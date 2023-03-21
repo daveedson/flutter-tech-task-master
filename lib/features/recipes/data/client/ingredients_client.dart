c8ec14cea107aa19239e49cba9db663716e7f921
David Onoh <daveeddrumz@gmail.com>
2023-03-21 08:34:21 +0100

    Created ingredients Client for making Api calls

---
+++ lib/features/recipes/data/client/ingredients_client.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:tech_task/features/recipes/domain/model/ingrident_response_model.dart';
+part 'ingredients_client.g.dart';
+
+@RestApi()
+abstract class IngredientClient {
+  factory IngredientClient(Dio dio, {String baseUrl}) = _IngredientClient;
+
+  @GET("ingredients")
+  Future<GetIngridentResponseModel> getIngredients();
+}
