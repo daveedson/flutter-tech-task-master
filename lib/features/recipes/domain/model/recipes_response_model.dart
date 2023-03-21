b32c84c7fa52e834fb0808ee0511583122c24c1a
David Onoh <daveeddrumz@gmail.com>
2023-03-21 02:28:17 +0100

    generated model class for receipe model

--- lib/features/recipes/domain/model/recipes_response_model.dart
+++ lib/features/recipes/domain/model/recipes_response_model.dart
@@ -1,34 +1,32 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:json_annotation/json_annotation.dart';
+part 'recipes_response_model.g.dart';
+
+@JsonSerializable(explicitToJson: true)
 class GetIngridentResponseModel {
   String? title;
+  @JsonKey(name: "use-by")
   String? useBy;
 
   GetIngridentResponseModel({this.title, this.useBy});
 
-  GetIngridentResponseModel.fromJson(Map<String, dynamic> json) {
-    title = json['title'];
-    useBy = json['use-by'];
-  }
+  factory GetIngridentResponseModel.fromJson(Map<String, dynamic> json) =>
+      _$GetIngridentResponseModelFromJson(json);
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['title'] = this.title;
-    data['use-by'] = this.useBy;
-    return data;
-  }
+  Map<String, dynamic> toJson() => _$GetIngridentResponseModelToJson(this);
 
   @override
   bool operator ==(covariant GetIngridentResponseModel other) {
     if (identical(this, other)) return true;
-  
-    return 
-      other.title == title &&
-      other.useBy == useBy;
+
+    return other.title == title && other.useBy == useBy;
   }
 
   @override
   int get hashCode => title.hashCode ^ useBy.hashCode;
 
   @override
-  String toString() => 'GetIngridentResponseModel(title: $title, useBy: $useBy)';
+  String toString() =>
+      'GetIngridentResponseModel(title: $title, useBy: $useBy)';
 }
---
+++ lib/features/recipes/domain/model/recipes_response_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'recipes_response_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+GetIngridentResponseModel _$GetIngridentResponseModelFromJson(
+        Map<String, dynamic> json) =>
+    GetIngridentResponseModel(
+      title: json['title'] as String?,
+      useBy: json['use-by'] as String?,
+    );
+
+Map<String, dynamic> _$GetIngridentResponseModelToJson(
+        GetIngridentResponseModel instance) =>
+    <String, dynamic>{
+      'title': instance.title,
+      'use-by': instance.useBy,
+    };
